ab336b0f2cb281c3aa5804733fb79c4407af15db
Jeff Verkoeyen <jverkoey@gmail.com>
2022-01-11 00:37:32 -0500

    Add url field.

--- Daydreamer.swiftpm/UI/Launcher/LauncherViewController.swift
+++ Daydreamer.swiftpm/UI/Launcher/LauncherViewController.swift
@@ -24,14 +24,40 @@ final class LauncherViewController: UIViewController {
         subtitleLabel.lineBreakMode = .byWordWrapping
         view.addSubview(subtitleLabel)
         
-        let urlField = UITextField()
+        let wellFrame = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        let urlFieldWell = UIView(frame: wellFrame)
+        urlFieldWell.translatesAutoresizingMaskIntoConstraints = false
+        urlFieldWell.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        urlFieldWell.backgroundColor = .secondarySystemFill
+        urlFieldWell.layer.borderWidth = 1
+        urlFieldWell.layer.cornerRadius = 32
+        urlFieldWell.clipsToBounds = true
+        
+        let urlField = UITextField(frame: wellFrame.insetBy(dx: 32, dy: 32))
+        urlField.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        urlField.placeholder = "Paste a Figma file url or file id"
+        urlField.textAlignment = .left
+        urlField.borderStyle = .none
+        urlField.font = UIFont(name: "Avenir Next Medium", size: 24)!
+        urlField.adjustsFontSizeToFitWidth = true
+        urlField.textColor = .label
+        urlFieldWell.addSubview(urlField)
+        view.addSubview(urlFieldWell)
         
         let layoutGuide = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
+            urlFieldWell.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            urlFieldWell.centerYAnchor.constraint(lessThanOrEqualTo: view.centerYAnchor),
+            urlFieldWell.bottomAnchor.constraint(lessThanOrEqualTo: view.keyboardLayoutGuide.topAnchor, constant: -32),
+            urlFieldWell.leadingAnchor.constraint(equalToSystemSpacingAfter: layoutGuide.leadingAnchor, multiplier: 4),
+            layoutGuide.trailingAnchor.constraint(equalToSystemSpacingAfter: urlFieldWell.trailingAnchor, multiplier: 4),
+            urlFieldWell.heightAnchor.constraint(equalToConstant: 100),
+            
             titleLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: layoutGuide.leadingAnchor, multiplier: 4),
             layoutGuide.trailingAnchor.constraint(equalToSystemSpacingAfter: titleLabel.trailingAnchor, multiplier: 4),
             subtitleLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: layoutGuide.leadingAnchor, multiplier: 4),
             layoutGuide.trailingAnchor.constraint(equalToSystemSpacingAfter: subtitleLabel.trailingAnchor, multiplier: 4),
+            
             subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
             layoutGuide.bottomAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 2),
         ])
@@ -66,5 +92,3 @@ final class LauncherViewController: UIViewController {
         print(size)
     }
 }
-
-// fjfjfjfjfjfjrfjffjfjfjfjfjffjffjffjfjfj
